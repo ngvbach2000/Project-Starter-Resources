b1e31f38fedc21fb6caf991c96773dc4cca43ee1
Bách Nguyễn <ngvbach2000@gmail.com>
2022-08-03 00:13:34 +0700

    update

--- terraform/modules/vm/vm.tf
+++ terraform/modules/vm/vm.tf
@@ -30,8 +30,8 @@ resource "azurerm_linux_virtual_machine" "vm" {
   }
   source_image_reference {
     publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = "16.04-LTS"
+    offer     = "0001-com-ubuntu-server-focal-daily"
+    sku       = "20_04-daily-lts-gen2"
     version   = "latest"
   }
 }
